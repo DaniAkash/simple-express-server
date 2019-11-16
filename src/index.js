206488435e5411d2c190956f187114368e6ccb21
Dani Akash <DaniAkash@users.noreply.github.com>
2019-11-16 09:15:38 +0530

    created index with pug

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
 const express = require("express");
 const fs = require("fs");
 
+/**
+ * uses the inbuilt path module to 
+ * do operations based on the OS
+ */
+const path = require("path");
+
+/**
+ * Creates the Express server
+ */
 const app = express();
 
+/**
+ * Users array that will be loaded with
+ * data from users.json file
+ */
+const users = [];
+
+/**
+ * Loading the users data
+ */
+fs.readFile(path.join(__dirname, '../users.json'), {encoding: 'utf8'}, (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+
+  JSON.parse(data).forEach(user => {
+    user.name.full = user.name.first + ' ' + user.name.last;
+    users.push(user);
+  });
+});
+
+/**
+ * Setting the views directory
+ * and the view engine to pug
+ * 
+ * __dirname gets the current directory of the file,
+ * it should be later joined with the directory in which the views are present
+ */
+app.set('views', path.join( __dirname, './views'));
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send("Hello, World!");
+
+  /**
+   * Will render a plain text
+   */
+  // res.send("Hello, World!");
+
+  /**
+   * Will render complicated html with the help of templates
+   * we created in the view directory
+   */
+  res.render("index", { users });
 });
 
 app.get('/welcome', (req, res) => {
   res.send("<h1>Welcome to express!</h1>");
 });
 
-const users = [];
+app.get(/crazy.*/, (req, res, next) => {
+  console.log("Crazy path excecuted!");
+  next();
+})
 
 app.get('/users', (req, res) => {
   let links = '';
@@ -22,16 +74,10 @@ app.get('/users', (req, res) => {
   res.send(links);
 })
 
-fs.readFile('/sandbox/users.json', {encoding: 'utf8'}, (err, data) => {
-  if (err) {
-    console.error(err);
-  }
-
-  JSON.parse(data).forEach(user => {
-    user.name.full = user.name.first + ' ' + user.name.last;
-    users.push(user);
-  });
-});
+app.get('/:user', (req, res) => {
+  const username = req.params.user;
+  res.send(username);
+})
 
 const server = app.listen(8000, () => {
   console.log("Server running in port: ", server.address().port)
