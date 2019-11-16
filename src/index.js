a1ef5f079cb18c8d58576f98f4e90f377b95fc39
Dani Akash <DaniAkash@users.noreply.github.com>
2019-11-16 08:52:31 +0530

    created users listing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-var http = require("http");
+const express = require("express");
+const fs = require("fs");
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get('/welcome', (req, res) => {
+  res.send("<h1>Welcome to express!</h1>");
+});
+
+const users = [];
+
+app.get('/users', (req, res) => {
+  let links = '';
+
+  users.forEach(function (user) {
+    links += '<a href="/' + user.username + '">' + user.name.full + '</a><br>'
   })
-  .listen(8080); //the server object listens on port 8080
+  res.send(links);
+})
+
+fs.readFile('/sandbox/users.json', {encoding: 'utf8'}, (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+
+  JSON.parse(data).forEach(user => {
+    user.name.full = user.name.first + ' ' + user.name.last;
+    users.push(user);
+  });
+});
+
+const server = app.listen(8000, () => {
+  console.log("Server running in port: ", server.address().port)
+});
